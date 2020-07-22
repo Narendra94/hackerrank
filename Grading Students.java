cc0eb6c99d807106e7450e412a9fba8036a3ddcc
Narendrapratap Yadav <narendrayadav494@gmail.com>
2020-07-22 18:46:20 +0530

    Update Grading Students.java

--- Grading Students.java
+++ Grading Students.java
@@ -1,11 +1,17 @@
-class Result {
+/*
+     HackerLand University has the following grading policy:
+    Every student receives a  in the inclusive range from  to .
+    Any  less than  is a failing grade.
+    Sam is a professor at the university and likes to round each student's  according to these rules:
 
-    /*
-     * Complete the 'gradingStudents' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts INTEGER_ARRAY grades as parameter.
+    If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+    If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+    For example,  will be rounded to  but  will not be rounded because the rounding would result in a number that is less than .
+
+    Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
      */
+
+class Result {
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
     // Write your code here
@@ -31,36 +37,4 @@ class Result {
 
     }
 
-}
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
-            try {
-                return bufferedReader.readLine().replaceAll("\\s+$", "");
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        })
-            .map(String::trim)
-            .map(Integer::parseInt)
-            .collect(toList());
-
-        List<Integer> result = Result.gradingStudents(grades);
-
-        bufferedWriter.write(
-            result.stream()
-                .map(Object::toString)
-                .collect(joining("\n"))
-            + "\n"
-        );
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
 }
